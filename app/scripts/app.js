3beaa736a822203c1e8f5e70ef5793947b658d07
Pramisha C P <pramishacp123@gmail.com>
2018-03-21 10:23:34 +0530

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+			templateUrl:'views/employeeViews/signup.html',
+			controller:'signupController'
+		})
+
+  });
